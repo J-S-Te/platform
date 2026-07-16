982208ba000610a554f678acc1a77db1772aa920
SyDan0day <1293047033@qq.com>
2026-07-16 18:49:25 +0800

    移动文件

--- migrations/embed.go
+++
@@ -1,9 +0,0 @@
-// Package migrations exposes the versioned MySQL migration files embedded in the binary.
-package migrations
-
-import "embed"
-
-// Files contains every versioned SQL migration in this directory.
-//
-//go:embed *.sql
-var Files embed.FS
