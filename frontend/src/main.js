1c81b3b6b2d3dafdf1d48982ded808e9d544e187
YGLF <2250900325@qq.com>
2026-07-24 14:25:16 +0800

    refactor/caixinhui/重构项目结构

--- frontend/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from '@/modules/platform/app/App.vue'
-import router from '@/router'
-import '@/modules/platform/shared/styles/main.css'
-
-createApp(App).use(router).mount('#app')
